04c5106a05d94545aa7e9d700ee657e8e394ea72
lagercat <malessebastian@gmail.com>
2019-08-24 14:56:28 +0300

    Fix database connection so that it maches current mongodb version

--- backend/connection.js
+++ backend/connection.js
@@ -3,18 +3,28 @@ const MongoClient = require('mongodb').MongoClient;
 class Connection {
   static connectToMongo() {
     if (this.db) return Promise.resolve(this.db);
-    return MongoClient.connect(this.url, this.options)
-        .then((db) => {
+    return MongoClient.connect(this.url, this.options,
+        (err, client) => {
+          if (err) {
+            throw err;
+          }
           console.log('Connected to database!');
-          this.db = db;
+          this.db = client.db(this.db_name);
+        });
+    /*
+        .then((err, client) => {
+          console.log('Connected to database!');
+          this.db = clien.db
         }).catch((error) => {
           console.log(errors);
         });
+    */
   }
 }
 
 Connection.db = null;
-Connection.url = 'mongodb://127.0.0.1:27017/hacker_news';
+Connection.url = 'mongodb://127.0.0.1:27017';
+Connection.db_name = 'hacker_news';
 Connection.options = {
   bufferMaxEntries: 0,
   reconnectTries: 5000,
