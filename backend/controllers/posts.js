4d5da1aacbec4c9a0eb16d3c7deec43f911bf553
lagercat <malessebastian@gmail.com>
2019-08-26 12:51:27 +0300

    Delete post route

--- backend/controllers/posts.js
+++ backend/controllers/posts.js
@@ -1,7 +1,7 @@
 const connection = require('../connection');
 const ObjectID = require('mongodb').ObjectId;
 
-exports.postCreate = (req, res, next) => {
+exports.createPost = (req, res, next) => {
   const db = connection.db;
   const collection = db.collection('posts');
   if (req.body.title && req.body.content
@@ -10,7 +10,7 @@ exports.postCreate = (req, res, next) => {
       title: req.body.title,
       content: req.body.content,
       type: req.body.type,
-      author: req.body.author,
+      author: req.userData.userId,
       date: new Date(),
     };
     collection.insertOne(post, (err, result) => {
@@ -32,7 +32,7 @@ exports.postCreate = (req, res, next) => {
   }
 };
 
-exports.getPost = (req, res, next) => {
+exports.readPost = (req, res, next) => {
   const db = connection.db;
   const collection = db.collection('posts');
   const postId = new ObjectID(req.params.id);
@@ -49,6 +49,39 @@ exports.getPost = (req, res, next) => {
       res.status(200).json({
         message: 'Post received successfully',
         post: result,
+      });
+    }
+  });
+};
+
+exports.deletePost = (req, res, next) => {
+  const db = connection.db;
+  const collection = db.collection('posts');
+  const postId = new ObjectID(req.params.id);
+  collection.findOne({_id: postId}, (err, result) => {
+    if (err) {
+      res.status(500).json({
+        message: err,
+      });
+    } else if (!result) {
+      res.status(404).json({
+        message: 'Post not found',
+      });
+    } else if (result.author != req.userData.userId) {
+      res.status(403).json({
+        message: 'Cannot delete post',
+      });
+    } else {
+      collection.deleteOne({_id: postId}, (err, result) => {
+        if (err) {
+          res.status(500).json({
+            message: err,
+          });
+        } else {
+          res.status(204).json({
+            message: 'Post deleted successfully',
+          });
+        }
       });
     }
   });
--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -4,7 +4,8 @@ const postsControllers = require('../controllers/posts');
 const routerObj = express.Router;
 const router = routerObj();
 
-router.post('/create', postsControllers.postCreate);
-router.get('/read/:id', postsControllers.getPost);
+router.post('/create', postsControllers.createPost);
+router.get('/read/:id', postsControllers.readPost);
+router.delete('/delete/:id', postsControllers.deletePost);
 
 module.exports = router;
