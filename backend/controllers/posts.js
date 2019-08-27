5b2276f68b11488e61993f82b8fc4b33b9f1acb9
lagercat <malessebastian@gmail.com>
2019-08-27 12:27:39 +0300

    Add update functionality and refactor delete

--- backend/controllers/posts.js
+++ backend/controllers/posts.js
@@ -1,9 +1,11 @@
 const connection = require('../connection');
 const ObjectID = require('mongodb').ObjectId;
 
+const sortingTypes = ['hot', 'new', 'top'];
+const timeIntervals = [1, 168, 720, 0]; // number of hours, 0 stands for all
+
 exports.createPost = (req, res, next) => {
-  const db = connection.db;
-  const collection = db.collection('posts');
+  const collection = connection.db.collection('posts');
   if (req.body.title && req.body.content
     && (req.body.type == 1 || req.body.type == 0)) {
     const post = {
@@ -12,6 +14,7 @@ exports.createPost = (req, res, next) => {
       type: req.body.type,
       author: req.userData.userId,
       date: new Date(),
+      points: 0,
     };
     collection.insertOne(post, (err, result) => {
       if (err) {
@@ -32,9 +35,49 @@ exports.createPost = (req, res, next) => {
   }
 };
 
+exports.updatePost = (req, res, next) => {
+  const collection = connection.db.collection('posts');
+  if (req.body.title && req.body.content
+    && (req.body.type == 1 || req.body.type == 0)) {
+    const post = {
+      $set: {
+        title: req.body.title,
+        content: req.body.content,
+        type: req.body.type,
+        author: req.userData.Id,
+        date: req.body.data,
+        points: req.body.points,
+      },
+    };
+    const postId = new ObjectID(req.params.id);
+    collection.updateOne({_id: postId, author: req.userData.Id}, post,
+        (err, result) => {
+          console.log(result);
+          if (err) {
+            console.log(err);
+            res.status(500).json({
+              message: err,
+            });
+          } else if (!result.modifiedCount) {
+            res.status(404).json({
+              message: 'Post not found',
+            });
+          } else {
+            res.status(204).json({
+              message: 'Post updated successfully',
+            });
+          }
+        }
+    );
+  } else {
+    res.status(401)({
+      message: 'Invalid credentials',
+    });
+  }
+};
+
 exports.readPost = (req, res, next) => {
-  const db = connection.db;
-  const collection = db.collection('posts');
+  const collection = connection.db.collection('posts');
   const postId = new ObjectID(req.params.id);
   collection.findOne({_id: postId}, (err, result) => {
     if (err) {
@@ -54,28 +97,73 @@ exports.readPost = (req, res, next) => {
   });
 };
 
+exports.readPosts = (req, res, next) => {
+  const collection = connection.db.collection('posts');
+  const pageNumber = parseInt(req.query.page);
+  const sortingType = req.query.sorting;
+  const timeInterval = parseInt(req.query.interval);
+  if (pageNumber <= 0 || !sortingTypes.includes(sortingType)
+    || !timeIntervals.includes(timeInterval)) {
+    res.status(401).json({
+      message: 'Invalid credentials',
+    });
+  } else {
+    const d = new Date();
+    d.setHours(d.getHours() - timeInterval);
+    const postsQuery = collection.find({
+      'date': {$gte: (timeInterval == 0 ? new Date(0): d)},
+    });
+    switch (sortingType) {
+      case 'new':
+        postsQuery.sort({'date': -1});
+        break;
+      case 'top':
+        postsQuery.sort({'points': -1});
+        break;
+      case 'hot':
+        postsQuery.map((post) => {
+          post.hotScore = parseInt(10 * Math.random()); // Will be changed
+          return post;
+        });
+        break;
+    }
+    postsQuery.toArray((error, result) => {
+      if (error) {
+        res.status(500).json({
+          message: error,
+        });
+      } else {
+        if (sortingType == 'hot') {
+          result.sort((a, b) => { // This sorting should be done at db level
+            if (a.hotScore < b.hotScore) {
+              return 1;
+            } else {
+              return -1;
+            }
+          });
+        }
+        res.status(200).json({
+          message: 'Posts read successfully',
+          posts: result,
+        });
+      }
+    });
+  }
+};
+
 exports.deletePost = (req, res, next) => {
-  const db = connection.db;
-  const collection = db.collection('posts');
+  const collection = connection.db.collection('posts');
   const postId = new ObjectID(req.params.id);
-  collection.findOne({_id: postId}, (err, result) => {
-    if (err) {
-      res.status(500).json({
-        message: err,
-      });
-    } else if (!result) {
-      res.status(404).json({
-        message: 'Post not found',
-      });
-    } else if (result.author != req.userData.userId) {
-      res.status(403).json({
-        message: 'Cannot delete post',
-      });
-    } else {
-      collection.deleteOne({_id: postId}, (err, result) => {
+  collection.deleteOne({_id: postId, author: req.userData.Id},
+      (err, result) => {
+        console.log(result);
         if (err) {
           res.status(500).json({
             message: err,
+          });
+        } else if (!result.deletedCount) {
+          res.status(404).json({
+            message: 'Post not found',
           });
         } else {
           res.status(204).json({
@@ -83,6 +171,4 @@ exports.deletePost = (req, res, next) => {
           });
         }
       });
-    }
-  });
 };
--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -6,6 +6,8 @@ const router = routerObj();
 
 router.post('/create', postsControllers.createPost);
 router.get('/read/:id', postsControllers.readPost);
+router.get('/read', postsControllers.readPosts);
+router.put('/update/:id', postsControllers.updatePost);
 router.delete('/delete/:id', postsControllers.deletePost);
 
 module.exports = router;
