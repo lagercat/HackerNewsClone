da77ee8644f04b1d475409561a9435e45eb67806
lagercat <malessebastian@gmail.com>
2019-08-29 16:37:03 +0300

    Votes routes fundament

---
+++ backend/controllers/votes.js
@@ -0,0 +1,4 @@
+const connection = require('../connection');
+
+exports.voteCreate = (req, res, next) => {
+};
---
+++ backend/routes/votes.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const votesControllers = require('../controllers/votes');
+
+const routerObj = express.Router;
+const router = routerObj();
+
+module.exports = router;
