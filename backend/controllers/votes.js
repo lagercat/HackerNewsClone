acc3a38c5ce0a6182f83651b5ffdb96a9b5ca216
lagercat <malessebastian@gmail.com>
2019-08-30 19:35:16 +0300

    Add voting routes

--- backend/controllers/votes.js
+++ backend/controllers/votes.js
@@ -1,4 +1,80 @@
 const connection = require('../connection');
+const ObjectID = require('mongodb').ObjectID;
 
 exports.createVote = (req, res, next) => {
+  const votesCollection = connection.db.collection('votes');
+  const vote = {
+    docType: parseInt(req.body.docType),
+    docId: req.body.docId,
+    date: new Date(),
+    author: req.userData.Id,
+  };
+  if (!(req.body.docType == 0 || req.body.docType == 1)) {
+    res.status(500).json({
+      message: 'Invalid data',
+    });
+  } else {
+    const collectionName = (req.body.docType == 0 ? 'comments' : 'posts');
+    const documentCollection = connection.db.collection(collectionName);
+    const documentId = new ObjectID(req.body.docId);
+    documentCollection.findOne({_id: documentId}, (err, result) => {
+      if (err) {
+        res.status(500).json({
+          message: err,
+        });
+      } else if (!result) {
+        res.status(404).json({
+          message: 'Object not found',
+        });
+      } else {
+        votesCollection.insertOne(vote, (err, _) => {
+          if (err) {
+            res.status(500).json({
+              message: err,
+            });
+          } else {
+            result.points++;
+            delete result._id;
+            const document = {
+              $set: result,
+            };
+            documentCollection.updateOne({_id: documentId}, document,
+                (err, _2) => {
+                  if (err) {
+                    res.status(500).json({
+                      message: err,
+                    });
+                  } else {
+                    res.status(201).json({
+                      message: 'Vote created',
+                    });
+                  }
+                }
+            );
+          }
+        });
+      }
+    });
+  }
+};
+
+exports.deleteVote = (req, res, next) => {
+  const collection = connection.db.collection('votes');
+  const voteId = new ObjectID(req.params.id);
+  collection.deleteOne({_id: voteId, author: req.userData.Id},
+      (err, result) => {
+        if (err) {
+          res.status(500).json({
+            message: err,
+          });
+        } else if (!result.deletedCount) {
+          res.status(404).json({
+            message: 'Vote not found',
+          });
+        } else {
+          res.status(204).json({
+            message: 'Vote deleted successfully',
+          });
+        }
+      });
 };
--- backend/routes/votes.js
+++ backend/routes/votes.js
@@ -4,4 +4,7 @@ const votesControllers = require('../controllers/votes');
 const routerObj = express.Router;
 const router = routerObj();
 
+router.post('/create', votesControllers.createVote);
+router.delete('/delete/:id', votesControllers.deleteVote);
+
 module.exports = router;
