a3b9548e43c217d918f179e2a02f44c01b638180
lagercat <malessebastian@gmail.com>
2019-09-10 14:53:44 +0300

    Posts service

---
+++ src/app/posts/posts.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Subject } from 'rxjs';
+
+const BACKEND_URL: string = environment.apiURL + '/posts/';
+
+@Injectable({
+    providedIn: 'root',
+})
+export class PostsService {
+    private postUpdated = new Subject();
+
+    constructor(private http: HttpClient) {}
+
+    getPosts(pageNumber: number, sortingType: string, timeInterval: number) {
+        const queryParams: string = '?page=${pageNumber}&sorting=${sortingType}&interval=' +
+            '${timeInterval}';
+        this.http.get<{message: string, posts: any}>(BACKEND_URL + queryParams).subscribe(
+            (postData) => {
+                console.log(postData);
+            });
+    }
+}
