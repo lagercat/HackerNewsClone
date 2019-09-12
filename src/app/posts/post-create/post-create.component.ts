8599c242cabb838460c846c0d7ee354692ef3661
lagercat <malessebastian@gmail.com>
2019-09-12 17:11:31 +0300

    Post create component

---
+++ src/app/posts/post-create/post-create.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-post-list',
+    templateUrl: './post-create.component.html',
+    styleUrls: ['./post-create.component.css'],
+})
+export class PostCreateComponent {
+    
+}
