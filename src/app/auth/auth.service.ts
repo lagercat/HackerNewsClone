4976f8a5c286ef565f948a8e74ec22397de87afc
lagercat <malessebastian@gmail.com>
2019-09-17 18:44:37 +0300

    Auth status

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -11,8 +11,23 @@ const BACKEND_URL = environment.apiURL + '/users/';
     providedIn: 'root',
 })
 export class AuthService {
+    private token: string = null;
+    private username: string = null;
+    private isAuthenticated = false;
 
     constructor(private httpClient: HttpClient, private router: Router) {}
+
+    getToken() {
+        return this.token;
+    }
+
+    getUsername() {
+        return this.username;
+    }
+
+    getIsAuthenticated() {
+        return this.isAuthenticated;
+    }
 
     createUser(formUsername: string, formPassword: string) {
         const payload = {
@@ -30,10 +45,59 @@ export class AuthService {
             username: formUsername,
             password: formPassword,
         };
-        this.httpClient.post<{message: string, token: string}>
+        this.httpClient.post<{message: string, token: string, user: User}>
         (BACKEND_URL + 'authenticate/', payload)
         .subscribe((response) => {
+            if (response.token != null) {
+                const token = response.token;
+                const username = response.user.username;
+                this.token = token;
+                this.username = username;
+                this.isAuthenticated =  true;
+                this.setAuthData(token, username);
+            }
             this.router.navigate(['/']);
         });
+    }
+
+    autoAuthUser() {
+        const authData = this.getAuthData();
+        const token = authData.token;
+        const username = authData.username;
+        if (token && username) {
+            this.isAuthenticated = true;
+            this.token = token;
+            this.username = username;
+        }
+    }
+
+    logout() {
+        this.isAuthenticated = false;
+        this.token = null;
+        this.username = null;
+        this.deleteAuthData();
+        this.router.navigate(['/']);
+    }
+
+    private setAuthData(token, username) {
+        localStorage.setItem('token', token);
+        localStorage.setItem('username', username);
+    }
+
+    private getAuthData() {
+        const localToken = localStorage.getItem('token');
+        const localUsername = localStorage.getItem('username');
+        if (!(localUsername && localToken)) {
+            return;
+        }
+        return {
+            token: localToken,
+            username: localUsername,
+        };
+    }
+
+    private deleteAuthData() {
+        localStorage.removeItem('token');
+        localStorage.removeItem('username');
     }
 }
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,9 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../auth/auth.service';
 
 @Component({
     selector: 'app-header',
     templateUrl: './header.component.html',
     styleUrls: ['./header.component.css'],
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
+    authStatus: boolean;
+
+    constructor(private authService: AuthService) {}
+
+    ngOnInit() {
+        this.authStatus = this.authService.getIsAuthenticated();
+    }
 }
